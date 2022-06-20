679845c7690958e7e055ffd7756eda50d9295a76
aditya1703s <62322649+aditya1703s@users.noreply.github.com>
2022-06-20 19:47:03 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following CSS style property is used to specify an italic text?",
+    answer: "font-style",
+    options: [
+      "style",
+      "font",
+      "font-style",
+      "@font-face"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is an appropriate value for the overflow element?",
+    answer: "all of the mentioned",
+    options: [
+      "scroll",
+      "hidden",
+      "auto",
+      "all of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following CSS property is used to specify table borders in CSS?",
+    answer: "border",
+    options: [
+      "table:border",
+      "table",
+      "border",
+      "none of the mentioned"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The domain of the function log( log sin(x) ) is",
+    answer: "Empty set",
+    options: [
+      "0 < x < π",
+      "Empty set",
+      "0 < x < π",
+      "2nπ < x < (2n + 1) π , for n in N "
+    ]
+  },
+    
+];
+
